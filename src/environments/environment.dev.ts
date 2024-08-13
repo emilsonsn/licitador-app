ef72a85d4e9f005b771ce141a3437b914e448cc9
emilsonsn <emilsonsn2@gmail.com>
2024-08-13 00:46:39 -0300

    fix: corrige environment

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: false,
   home: '/painel',
   appName: 'Licitador App',
-  api: 'http://127.0.0.1:8000/api',
+  api: 'https://app.localizadordeeditais.com.br:3001/api',
   url: '',
   version: require('../../package.json').version,
   phoneNumber: '5551999999999',
