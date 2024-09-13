77386856c848cb8e29273ba20863885f761c9e04
emilsonsn <emilsonsn2@gmail.com>
2024-09-12 21:38:39 -0300

    feat: adiciona url para suporte

--- src/app/views/private/help/help.component.ts
+++ src/app/views/private/help/help.component.ts
@@ -10,4 +10,12 @@ export class HelpComponent {
   link: string = `https://wa.me/${environment.phoneNumber}`;
   phoneNumber: string = environment.phoneNumber;
 
+  ngOnInit() {
+    // Abre o site em outra aba
+    window.open('https://licitanteprime.com.br/ajuda/', '_blank');
+  
+    // Redireciona a aba atual para a home
+    window.location.href = '/';
+  }
+  
 }
